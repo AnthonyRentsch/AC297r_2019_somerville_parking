5edfb833fc64be2fb1b3198b6c8777275ca20d77
Anthony Rentsch <anthony.rentsch@gmail.com>
2019-11-22 17:53:11 -0500

    model now loads, df is created properly. need to get predictions properly

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,16 @@
 # imports
+import warnings
+warnings.filterwarnings('ignore')
 import tensorflow as tf
+from tensorflow.keras import optimizers
 import pandas as pd
 import numpy as np
 import glob
+
 import sys
 sys.path.append('../')
-from generator import generator_two_inputs
+from generator import generator_three_inputs
+from models import three_input_model
 from metrics import sensitivity, specificity
 
 
@@ -22,13 +27,26 @@ def load_model(model_path):
 	model : Keras model object
 	'''
 
-	model = tf.keras.models.load_model(model_path, 
-		custom_objects={'sensitivity':sensitivity, 'specificity':specificity})
+	# redefine optimizer used when model was compiled
+	adam = optimizers.Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, amsgrad=False)
+
+	# load model without compiling
+	model = tf.keras.models.load_model(model_path,
+		custom_objects={'sensitivity':sensitivity, 'specificity':specificity},
+		compile = False)
+	
+	# compile model with correct loss function and optimizer
+	model.compile(loss = 'categorical_crossentropy', optimizer=adam)
+
 	return model
 
 def predict(model, data, y_column='temp_label',
-	aerial_dir='../data/training/aerial_images/', gsv_dir='../data/training/street_view_images/',
-	gsv_image_dim = (128,128, 3), aer_image_dim = (128,128, 4)):
+	aerial_dir='../data/training/aerial_images/', 
+	gsv_dir='../data/training/street_view_images/',
+	tabular_path='../data/residence_addresses_googlestreetview_clean.csv',
+	tabular_predictor_cols=None,
+	gsv_image_dim = (128,128, 3), 
+	aer_image_dim = (128,128, 4)):
 	'''
 	Generates model predictions for data.
 
@@ -47,15 +65,25 @@ def predict(model, data, y_column='temp_label',
 	preds : np.array
 	'''
 
-	preds = model.predict_generator(generator_two_inputs(data, aerial_dir = aerial_dir, gsv_dir = gsv_dir, 
+	# load tabular data
+	tabular_df = pd.read_csv(tabular_path)
+	cols = list(tabular_df.columns ^ ['MBL']) # TEMP
+
+	# create generator
+	gen = generator_three_inputs(data, 
+		tabular_data = tabular_df,
+		tabular_predictor_cols = cols, #tabular_predictor_cols,
+		aerial_dir = aerial_dir, gsv_dir = gsv_dir, 
 		batch_size = data.shape[0], gsv_image_dim = gsv_image_dim, aer_image_dim = aer_image_dim,
-		y_column = y_column),
-	steps = 1)
+		y_column = y_column)
+
+	# predict using this generator
+	preds = model.predict_generator(gen, steps = 1)
 
 	return preds
 
-def create_parcel_df(aerial_dir='../data/training/aerial_images/', 
-	gsv_dir='../data/training/sv_images/'):
+def create_parcel_df(aerial_dir='../data/training/aerial_images/', gsv_dir='../data/training/sv_images/',
+					 temp_label_col = 'temp_label'):
 	'''
 	Creates dataframe to pass into predict function for all parcels in Somerville.
 
@@ -63,6 +91,8 @@ def create_parcel_df(aerial_dir='../data/training/aerial_images/',
 	----------
 	aerial_dir : str
 	gsv_dir : str
+	temp_label_col : str
+		Name of column to use for label. Label is not real but specificying one is needed for data generator.
 
 	Returns
 	-------
@@ -83,13 +113,16 @@ def create_parcel_df(aerial_dir='../data/training/aerial_images/',
 	gsv_df = pd.DataFrame(gsv_files_raw, columns=['gsv_ADDR'])
 	df = aerial_df.merge(gsv_df, how='outer', left_on='aerial_ADDR', right_on='gsv_ADDR')
 
+	# append temp label column
+	df[temp_label_col] = '1'
+
 	return df
 
 if __name__ == '__main__':
 
-	# # load model
-	# model_path = '../models/basicmodel.h5'
-	# model = load_model(model_path)
+	# load model
+	model_path = '../models/imageandtabular_model.h5'#'../models/satellite_model.h5'#
+	model = load_model(model_path)
 
 	# # load data
 	# data = pd.read_csv('../labels/training_labels_updated.csv')
@@ -109,5 +142,11 @@ if __name__ == '__main__':
 	# print(f"\nThere are {preds.sum()} drvieways.")
 
 	df = create_parcel_df()
+	
+	preds = predict(model, df)
+
+	print(preds)
+
+	# currently no street view images saved
 
 

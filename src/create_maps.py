46f1694157921cb4214408de732005767808dcfe
Anthony Rentsch <anthony.rentsch@gmail.com>
2019-12-08 16:12:47 -0500

    added block level bin preds mapping code

--- src/create_maps.py
+++ src/create_maps.py
@@ -3,26 +3,22 @@ import geopandas
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import numpy as np
 import matplotlib as mpl
 
-def create_label_map(labels, save_path='../images/label_map.png', parcels_path='../data/Parcels_FY19'):
+def create_label_map(labels, parcels, save_path='../images/label_map.png'):
 	'''
 	Create map of where we labeled data.
 
 	Parameters
 	----------
 	labels : pd.DataFrame
+	parcels : geopandas.DataFrame
 	save_path : str
-	parcels_path : str
 
 	Returns
 	-------
 	None
 	'''
-
-	# load data
-	parcels = geopandas.read_file(parcels_path)
 
 	# merge
 	parcel_labels = parcels.merge(labels, how='left', left_on='TaxParMBL', right_on='MBL')
@@ -42,17 +38,17 @@ def create_label_map(labels, save_path='../images/label_map.png', parcels_path='
 	plt.savefig(save_path, dpi=450)
 	
 
-def create_predictions_map(save_path='../images/predictions_map.png',
-	parcels_path='../data/Parcels_FY19', 
-	predictions_path='../data/driveway_predictions.csv'):
+def create_predictions_map(parcels, predictions, col, bins=None, save_path='../images/predictions_map.png'):
 	'''
 	Create map with parcel-level predictions.
 
 	Parameters
 	----------
+	parcels : geopandas.DataFrame
+	predictions : pd.DataFrame
+	col : str
+	bins : list
 	save_path : str
-	parcels_path : str
-	predictions_path : str
 
 	Returns
 	-------
@@ -60,25 +56,39 @@ def create_predictions_map(save_path='../images/predictions_map.png',
 	'''
 
 	# load data
-	parcels = geopandas.read_file(parcels_path)
-	predictions = pd.read_csv(predictions_path)
 	parcel_df_driveways = parcels.merge(predictions, how='left', left_on='TaxParMBL', right_on='MBL')
+	print(parcel_df_driveways.head())
 
 	# instantiate map
 	fig, ax = plt.subplots(figsize=(20,20))
+	parcel_df_driveways.plot(ax=ax, color='white', edgecolor='white', alpha=0.1)
 
 	# plot
-	parcel_df_driveways.plot(ax=ax, color='white', edgecolor='white', alpha=0.1)
-	parcel_df_driveways.plot(column='driveway_yes', ax=ax, cmap='Reds', alpha=0.7)	  
 
-	# Create colorbar as a legend
-	sm = plt.cm.ScalarMappable(cmap='Reds', norm=plt.Normalize(vmin=0, vmax=1))
-	# empty array for the data range
-	sm._A = []
-	# add the colorbar to the figure
-	cbar = fig.colorbar(sm, shrink=0.5, orientation='horizontal', pad=0)
+	if bins: # discrete
 
-	plt.axis('off')
+		parcel_df_driveways[parcel_df_driveways[col].notnull()].plot(ax=ax, column=col, 
+			cmap='Reds', scheme='user_defined', classification_kwds={'bins':bins}, legend=False)
+
+		cmap = plt.cm.Reds 
+		cmaplist = [cmap(i) for i in range(cmap.N)]
+		cmap = mpl.colors.LinearSegmentedColormap.from_list(
+		    'Custom cmap', cmaplist, cmap.N)
+		norm = mpl.colors.BoundaryNorm(bins, cmap.N)
+
+		ax_cm = fig.add_axes([0.25, 0.05, 0.5, 0.05]) #left bottom width height
+		cbar = mpl.colorbar.ColorbarBase(ax_cm, cmap=cmap, norm=norm, orientation='horizontal',
+		    spacing='uniform', ticks=bins, boundaries=bins)
+
+	else: # continuous
+		
+		parcel_df_driveways.plot(column=col, ax=ax, cmap='Reds', alpha=0.7)	  
+
+		sm = plt.cm.ScalarMappable(cmap='Reds', norm=plt.Normalize(vmin=0, vmax=1))
+		sm._A = []
+		cbar = fig.colorbar(sm, shrink=0.5, orientation='horizontal', pad=0)
+
+	ax.axis('off')
 
 	# save
 	plt.savefig(save_path, dpi=450)
@@ -87,12 +97,25 @@ def create_predictions_map(save_path='../images/predictions_map.png',
 
 if __name__ == '__main__':
 
-	# read labels
+	# read in data
 	manual_labels = pd.read_csv('../labels/training_labels_updated_111219.csv')
 	additional_manual_labels = pd.read_csv('../labels/additional_training_labels_120319.csv')
 	labels = pd.concat([manual_labels, additional_manual_labels], axis=0, sort=False)
+	parcels = geopandas.read_file('../data/Parcels_FY19')
+	calibrated_predictions = pd.read_csv('../data/calibrated_driveway_predictions.csv')
 
 	# create maps
-	create_label_map(labels)
-	create_predictions_map()
+	create_label_map(labels, parcels)
+	create_predictions_map(parcels, calibrated_predictions, 'calibrated_yes_driveway')
+
+	# aggregate at block-level and produce new map with discrete scale
+	# CAUTION - this breaks when running from command line - run from Jupyter notebook to be safe
+	calibrated_predictions['block'] = calibrated_predictions['MBL'].apply(lambda x: x.split('-')[0] + x.split('-')[1])
+	block_pred_counts = calibrated_predictions.groupby('block').mean()['calibrated_yes_driveway'].to_frame().reset_index()
+	block_pred_counts.columns = ['block', 'block_probs']
+	block_calibrated_predictions = calibrated_predictions.merge(block_pred_counts, how='left', left_on='block', right_on='block')
+
+	bins = [0.1,0.5,0.6,0.7,0.8,0.9,1]
+	create_predictions_map(parcels, block_calibrated_predictions, bins, 
+		'block_probs', '../images/block_predictions_map.png')
 
